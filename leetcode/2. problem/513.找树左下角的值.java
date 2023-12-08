a9e9aaf1fe3a429bf1572e4c1dede23c0cb118c3
krest <18635572970@163.com>
2023-12-09 00:16:30 +0800

    12.8

--- leetcode/2. problem/513.找树左下角的值.java
+++ leetcode/2. problem/513.找树左下角的值.java
@@ -26,14 +26,37 @@ import entrty.TreeNode;
  */
 class Solution {
     public int findBottomLeftValue(TreeNode root) {
+        // 层层倒序遍历
         // 使用BFS算法
-        return extracted(root);
+        // return extracted(root);
+
+        //
+        return test(root);
+    }
+
+    private int test(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return root.val;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        TreeNode temp = new TreeNode(-100);
+        while (!queue.isEmpty()) {
+            temp = queue.poll();
+            if (temp.right != null) {
+                queue.offer(temp.right);
+            }
+            if (temp.left != null) {
+                queue.offer(temp.left);
+            }
+        }
+        return temp.val;
     }
 
     /**
-     * 76/76 cases passed (2 ms)
-     * Your runtime beats 15.16 % of java submissions
-     * Your memory usage beats 5.04 % of java submissions (41.4 MB)
+     * 77/77 cases passed (1 ms)
+     * Your runtime beats 61.76 % of java submissions
+     * Your memory usage beats 5.02 % of java submissions (43.2 MB)
      */
     private int extracted(TreeNode root) {
         if (root.left == null && root.right == null) {
