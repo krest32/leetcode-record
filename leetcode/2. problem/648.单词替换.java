c02937d0f4ffb878569568e8c8e02a08829a0bf7
DuXin <duxin@ysstech.com>
2023-12-21 16:57:13 +0800

    12.21

--- leetcode/2. problem/648.单词替换.java
+++ leetcode/2. problem/648.单词替换.java
@@ -12,24 +12,62 @@ import java.util.Set;
 class Solution {
     public String replaceWords(List<String> roots, String sentence) {
         // 模拟
-        return extracted(roots, sentence);
+        // return extracted(roots, sentence);
+
+        return test(roots, sentence);
     }
 
-    private String extracted(List<String> roots, String sentence) {
-        Set<String> rootset = new HashSet<>();
+    private String test(List<String> roots, String sentence) {
+        Set<String> rootSet = new HashSet<>();
         for (String root : roots) {
-            rootset.add(root);
+            rootSet.add(root);
         }
+
         StringBuilder ans = new StringBuilder();
         for (String word : sentence.split("\\s+")) {
             String prefix = "";
-            for (int i = 1; i <= word.length(); ++i) {
+            for (int i = 1; i <= word.length(); i++) {
                 prefix = word.substring(0, i);
-                if (rootset.contains(prefix))
+                if (rootSet.contains(prefix)) {
                     break;
+                }
             }
-            if (ans.length() > 0)
+            if (ans.length() > 0) {
                 ans.append(" ");
+            }
+            ans.append(prefix);
+        }
+        return ans.toString();
+    }
+
+    /**
+     * 127/127 cases passed (858 ms)
+     * Your runtime beats 5.11 % of java submissions
+     * Your memory usage beats 15.03 % of java submissions (54.2 MB)
+     * 
+     * @param roots
+     * @param sentence
+     * @return
+     */
+
+    private String extracted(List<String> roots, String sentence) {
+        Set<String> rootSet = new HashSet<>();
+        for (String root : roots) {
+            rootSet.add(root);
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (String word : sentence.split("\\s+")) {
+            String prefix = "";
+            for (int i = 1; i <= word.length(); i++) {
+                prefix = word.substring(0, i);
+                if (rootSet.contains(prefix)) {
+                    break;
+                }
+            }
+            if (ans.length() > 0) {
+                ans.append(" ");
+            }
             ans.append(prefix);
         }
         return ans.toString();
